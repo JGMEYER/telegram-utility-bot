d45799ff81ab8c82706782a3736c688cabf37176
JGMEYER <meyer.justian@gmail.com>
2019-05-17 11:46:33 -0400

    Add YouTube API search

---
+++ google.py
@@ -0,0 +1,26 @@
+import os
+
+import httplib2
+from googleapiclient.discovery import build
+
+# Log all request/response headers and bodies
+httplib2.debuglevel = 4
+
+def search_youtube(q, max_results):
+    youtube = build('youtube', 'v3',
+      developerKey=os.environ.get('YOUTUBE_API_KEY'))
+
+    search_response = youtube.search().list(
+        q=q,
+        part="id,snippet",
+        maxResults=max_results,
+    ).execute()
+
+    videos = []
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                       search_result["id"]["videoId"]))
+    return videos
+
+print(search_youtube(q="G.o.a.t polyphia", max_results=5))
