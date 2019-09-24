0f32e12a30ca54b52d506ff4588e8bc0e1555471
JGMEYER <meyer.justian@gmail.com>
2019-09-24 16:50:41 -0400

    Ignore old updates and add url match logging

--- handler.py
+++ handler.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 import os
@@ -33,7 +34,8 @@ def handler(event, context):
             return send_alert(event, context)
         elif event['path'] == "/webhookUpdate":
             return handle_webhook_update(event, context)
-    except Exception:
+    except Exception as e:
+        logging.error("Handling request", exc_info=True)
         return {"statusCode": 500}
     return {"statusCode": 404}
 
@@ -61,9 +63,23 @@ def handle_webhook_update(event, context):
     event_body = json.loads(event['body'])
 
     try:
+        msg_date = event_body['message']['date']
+    except KeyError:
+        logging.error("Parsing date from Telegram update")
+        return {"statusCode": 400}
+
+    # avoid spamming our APIs
+    timeout = 30  # seconds
+    age = datetime.datetime.now().timestamp() - msg_date
+    logging.info(f"Telegram update age: {age}s")
+    if age > timeout:
+        logging.info(f"Ignoring old Telegram update")
+        return {"statusCode": 200}
+
+    try:
         text = event_body['message']['text']
     except KeyError:
-        logging.error("Parsing message from Telegram update")
+        logging.error("Parsing text from Telegram update")
         return {"statusCode": 400}
 
     urls = urls_in_text(text)
@@ -81,7 +97,10 @@ Webhook Update Parsers
 def send_music_mirror_links(urls):
     similar_tracks = get_similar_tracks_from_urls(urls)
     if not similar_tracks:
+        logging.info("No mirrors found for url")
         return {"statusCode": 200}
+
+    logging.info(f"similar_tracks: {similar_tracks}") #TODO rm
 
     response = "Mirrors:\n"
     for idx, track_matches in enumerate(similar_tracks):
@@ -121,17 +140,20 @@ def urls_in_text(text):
 
 def get_similar_tracks_from_urls(urls):
     similar_tracks: List[Dict[str, StreamingServiceTrack]] = []
+    logging.info(f"urls: {urls}") #TODO rm
     for url in urls:
         logging.info("URL detected")
         logging.info(url)
         svc = get_streaming_service_for_url(url)
         if svc:
             trackId = svc.get_trackId_from_url(url)
+            logging.info(f"url: {url} ; trackId: {trackId}") #TODO rm
             try:
                 with svc() as svc_client:
                     original_track = svc_client.get_track_from_trackId(trackId)
             except Exception as e:
                 logging.error("Getting track from track id", exc_info=True)
+                continue
             similar_tracks.append(get_similar_tracks_for_original_track(svc, original_track))
         else:
             logging.info("URL is not for a supported streaming service")
