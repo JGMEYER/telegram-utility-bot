a5a9981416a4f08444a2aa13134b44fd87a91ca9
JGMEYER <meyer.justian@gmail.com>
2020-07-06 08:16:42 -0400

    Add more regex matches and cleanup tests

--- streaming/streaming.py
+++ streaming/streaming.py
@@ -66,18 +66,22 @@ class StreamingServiceTrack(metaclass=ABCMeta):
             f"({self.id})"
         )
 
+    # Expressions that impact our ability to effectively match between
+    # different services.
     TITLE_EXCLUDE_EXPRESSIONS = [
         r"\s\(?(HD\s?)?((with |w\/ )?lyrics)?\)?$",  # ()'s
         r"\s\[?(HD\s?)?((with |w\/ )?lyrics)?\]?$",  # []'s
         r"\((Official\s)?(Music\s|Lyric\s)?(Video|Movie|Audio)\)",  # ()'s
         r"\[(Official\s)?(Music\s|Lyric\s)?(Video|Movie|Audio)\]",  # []'s
-        r"\s\(.*Live(\sat|\son|\sin)?.*\)",  # ()'s
-        r"\s\[.*Live(\sat|\son|\sin)?.*\]",  # []'s
-        # TODO add matches for "(Original Mix)"
-        # TODO add matches for "(Official)"
+        r"\s\(.*Live( at| on| in)?.*\)",  # ()'s
+        r"\s\[.*Live( at| on| in)?.*\]",  # []'s
+        r"\s\((Original|Official)( Mix)?\)",  # ()'s
+        r"\s\[(Original|Official)( Mix)?\]",  # []'s
+        r"\s\(Remaster(ed)?\)",  # ()'s
+        r"\s\[Remaster(ed)?\]",  # []'s
+        r"\s\((Feat\.?|Featuring)\s.*\)",  # ()'s
+        r"\s\[(Feat\.?|Featuring)\s.*\]",  # []'s
     ]
-
-    # Expressions to exclude:
 
     @abstractproperty
     def title(self):
--- streaming/tests/unit/test_streaming.py
+++ streaming/tests/unit/test_streaming.py
@@ -25,170 +25,113 @@ class TestStreamingServiceTrack(TestCase):
     def test_cleaned_title(self):
         mock_sst = MockStreamingServiceTrack(None, None, None)
 
+        def _assert_cleaned(title):
+            mock_sst.title = title
+            self.assertEqual(mock_sst.cleaned_title, "Song")
+            mock_sst.title = None
+
         # r"\s\(?(HD\s?)?((with |w\/ )?lyrics)?\)?$"
 
-        mock_sst.title = "Song (HD With Lyrics)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (HD W/ Lyrics)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (With Lyrics)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (W/ Lyrics)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (HD With Lyrics)")
+        _assert_cleaned("Song (HD W/ Lyrics)")
+        _assert_cleaned("Song (With Lyrics)")
+        _assert_cleaned("Song (W/ Lyrics)")
 
         # r"\s\[?(HD\s?)?((with |w\/ )?lyrics)?\]?$"
 
-        mock_sst.title = "Song [HD With Lyrics]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [HD W/ Lyrics]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [With Lyrics]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [W/ Lyrics]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [HD With Lyrics]")
+        _assert_cleaned("Song [HD W/ Lyrics]")
+        _assert_cleaned("Song [With Lyrics]")
+        _assert_cleaned("Song [W/ Lyrics]")
 
         # r"\((Official\s)?(Music\s|Lyric\s)?(Video|Movie|Audio)\)"
 
-        mock_sst.title = "Song (Official Music Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Music Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Music Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Lyric Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Lyric Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Lyric Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Official Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Music Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Music Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Music Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Lyric Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Lyric Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Lyric Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Video)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Movie)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Audio)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (Official Music Video)")
+        _assert_cleaned("Song (Official Music Movie)")
+        _assert_cleaned("Song (Official Music Audio)")
+        _assert_cleaned("Song (Official Lyric Video)")
+        _assert_cleaned("Song (Official Lyric Movie)")
+        _assert_cleaned("Song (Official Lyric Audio)")
+        _assert_cleaned("Song (Official Video)")
+        _assert_cleaned("Song (Official Movie)")
+        _assert_cleaned("Song (Official Audio)")
+        _assert_cleaned("Song (Music Video)")
+        _assert_cleaned("Song (Music Movie)")
+        _assert_cleaned("Song (Music Audio)")
+        _assert_cleaned("Song (Lyric Video)")
+        _assert_cleaned("Song (Lyric Movie)")
+        _assert_cleaned("Song (Lyric Audio)")
+        _assert_cleaned("Song (Video)")
+        _assert_cleaned("Song (Movie)")
+        _assert_cleaned("Song (Audio)")
 
         # r"\[(Official\s)?(Music\s|Lyric\s)?(Video|Movie|Audio)\]"
 
-        mock_sst.title = "Song [Official Music Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [Official Music Video]")
+        _assert_cleaned("Song [Official Music Movie]")
+        _assert_cleaned("Song [Official Music Audio]")
+        _assert_cleaned("Song [Official Lyric Video]")
+        _assert_cleaned("Song [Official Lyric Movie]")
+        _assert_cleaned("Song [Official Lyric Audio]")
+        _assert_cleaned("Song [Official Video]")
+        _assert_cleaned("Song [Official Movie]")
+        _assert_cleaned("Song [Official Audio]")
+        _assert_cleaned("Song [Music Video]")
+        _assert_cleaned("Song [Music Movie]")
+        _assert_cleaned("Song [Music Audio]")
+        _assert_cleaned("Song [Lyric Video]")
+        _assert_cleaned("Song [Lyric Movie]")
+        _assert_cleaned("Song [Lyric Audio]")
+        _assert_cleaned("Song [Video]")
+        _assert_cleaned("Song [Movie]")
+        _assert_cleaned("Song [Audio]")
 
-        mock_sst.title = "Song [Official Music Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\(.*Live( at| on| in)?.*\)"
 
-        mock_sst.title = "Song [Official Music Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (Live)")
+        _assert_cleaned("Song (Live at Bonnaroo)")
+        _assert_cleaned("Song (Live on Stage)")
+        _assert_cleaned("Song (Recorded Live in San Francisco)")
 
-        mock_sst.title = "Song [Official Lyric Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\[.*Live( at| on| in)?.*\]"
 
-        mock_sst.title = "Song [Official Lyric Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [Live]")
+        _assert_cleaned("Song [Live at Bonnaroo]")
+        _assert_cleaned("Song [Live on Stage]")
+        _assert_cleaned("Song [Recorded Live in San Francisco]")
 
-        mock_sst.title = "Song [Official Lyric Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\((Original|Official)( Mix)?\)"
 
-        mock_sst.title = "Song [Official Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (Original)")
+        _assert_cleaned("Song (Official)")
+        _assert_cleaned("Song (Original Mix)")
+        _assert_cleaned("Song (Official Mix)")
 
-        mock_sst.title = "Song [Official Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\[(Original|Official)( Mix)?\]"
 
-        mock_sst.title = "Song [Official Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [Original]")
+        _assert_cleaned("Song [Official]")
+        _assert_cleaned("Song [Original Mix]")
+        _assert_cleaned("Song [Official Mix]")
 
-        mock_sst.title = "Song [Music Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\(Remaster(ed)?\)"
 
-        mock_sst.title = "Song [Music Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (Remaster)")
+        _assert_cleaned("Song (Remastered)")
 
-        mock_sst.title = "Song [Music Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\[Remaster(ed)?\]"
 
-        mock_sst.title = "Song [Lyric Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [Remaster]")
+        _assert_cleaned("Song [Remastered]")
 
-        mock_sst.title = "Song [Lyric Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\((Feat\.?|Featuring)\s.*\)"
 
-        mock_sst.title = "Song [Lyric Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song (Feat Calvin & Hobbes)")
+        _assert_cleaned("Song (Feat. Calvin & Hobbes)")
+        _assert_cleaned("Song (Featuring Calvin & Hobbes)")
 
-        mock_sst.title = "Song [Video]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        # r"\s\[(Feat\.?|Featuring)\s.*\]"
 
-        mock_sst.title = "Song [Movie]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [Audio]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        # r"\s\(.*Live (at|on).*\)"
-
-        mock_sst.title = "Song (Live)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Live at Bonnaroo)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Live on Stage)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song (Recorded Live in San Francisco)"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        # r"\s\[.*Live (at|on).*\]"
-
-        mock_sst.title = "Song [Live]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [Live at Bonnaroo]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [Live on Stage]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
-
-        mock_sst.title = "Song [Recorded Live in San Francisco]"
-        self.assertEqual(mock_sst.cleaned_title, "Song")
+        _assert_cleaned("Song [Feat Calvin & Hobbes]")
+        _assert_cleaned("Song [Feat. Calvin & Hobbes]")
+        _assert_cleaned("Song [Featuring Calvin & Hobbes]")
