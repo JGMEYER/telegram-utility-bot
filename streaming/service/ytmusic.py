cd57c9ed299e16f2ad1d32a950cb078bc1068b9b
JGMEYER <meyer.justian@gmail.com>
2020-07-14 07:38:44 -0400

    Cleanup failed sts finder code

--- streaming/service/ytmusic.py
+++ streaming/service/ytmusic.py
@@ -1,8 +1,3 @@
-# TODO rm
-# import re
-# import requests
-# import urllib.parse
-# from bs4 import BeautifulSoup
 import os
 
 from googleapiclient.discovery import build
@@ -49,64 +44,6 @@ class YTMusicTrack(StreamingServiceTrack):
     def share_link(self):
         """WARNING: This is not going through an API and is subject to break"""
         return f"https://music.youtube.com/watch?v={self.id}"
-
-
-# TODO this may all be replaceable by following the first step of
-# https://www.youtube.com/get_video_info?video_id=e_S9VvJM1PI
-# i.e. just call https://www.youtube.com/get_video_info?video_id={videoId}
-
-# class STSFinder:
-#     """To call the YouTube's reverse-engineered get_video_info/ endpoint, we
-#     need an `sts` value. This class is dedicated to fetching this value.
-#     """
-
-#     # TODO rm
-#     # EMBED_PLAYER_JS_PATTERN = (
-#     #     r"/yts/jsbin/www-embed-player-(?P<trackId>\w+)/www-embed-player.js"
-#     # )
-
-#     @classmethod
-#     def get_sts(cls, trackId):
-#         embed_html = cls._get_video_embed_page_html(trackId)
-#         base_js_url = cls._get_base_js_url(embed_html)
-#         sts = cls._extract_sts_from_base_js(base_js_url)
-
-#         return sts  # TODO
-
-#     @classmethod
-#     def _get_video_embed_page_html(cls, trackId):
-#         page = requests.get(f"https://www.youtube.com/embed/{trackId}")
-#         return page.text
-
-#     @classmethod
-#     def _get_base_js_url(cls, embed_html):
-#         soup = BeautifulSoup(embed_html, "html.parser")
-#         script_tag = soup.find(
-#             "script", {"name": "www-embed-player/www-embed-player"}
-#         )
-#         embed_player_path = script_tag["src"]
-
-#         # Extract playerid from player path
-#         match = re.search(
-#             r"/yts/jsbin/www-embed-player-(?P<player_id>\w+)/www-embed-player.js",
-#             embed_player_path,
-#         )
-#         if not match:
-#             raise Exception(
-#                 "Could not extract player_id from embed_player_path"
-#             )
-#         player_id = match.group("player_id")
-
-#         return f"https://www.youtube.com/yts/jsbin/player-{player_id}/en_US/base.js"
-
-#     @classmethod
-#     def _extract_sts_from_base_js(cls, base_js_url):
-#         print(base_js_url)
-#         page = requests.get(base_js_url)
-#         match = re.search(r"\,sts\:(?P<sts>\d+)", page.text)
-#         # print(page.text)
-#         print(match)
-#         return match
 
 
 class YTMusic(StreamingService):
